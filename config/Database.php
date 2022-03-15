d58d266ea0e912e91588be2adaca86609be61641
Megan <mleavens19@gmail.com>
2022-03-15 18:09:11 -0400

    Second commit

--- config/Database.php
+++ config/Database.php
@@ -1,16 +1,16 @@
 <?php
 class Database {
 
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
+    private $host = 'm7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
+    private $db_name = 'w5huh52nmonaqr7h';
+    private $username = 'y8r135ms02a89dsf';
+    private $password = getenv('PASSWORD');
     private $conn;
 
     public function connect(){
         $this->conn = null;
-        // $url = getenv('JAWSDB_URL');
-        // $dbparts = parse_url($url);
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
 
         try {
             $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+
+?>
+<h1>Midterm Project!</h1>
