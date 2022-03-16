e2f6dcb576778d58ed51f5017132dc08aced2b3f
Megan <mleavens19@gmail.com>
2022-03-15 20:41:26 -0400

    readding them

--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,12 @@
 <?php
 class Database {
 
+    private $hostname; 
+    private $database;
+    private $username;
+    private $password;
+    private $conn;
+
     public function connect(){
         $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
@@ -11,15 +17,15 @@ class Database {
         $database = ltrim($dbparts['path'],'/');
 
         try {
-    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+            $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            }
+        catch(PDOException $e)
+            {
+            echo "Connection failed: " . $e->getMessage();
+            }
 }
 
 }
