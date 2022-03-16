fc511b179780ce4bfe991de9fd45e95415278715
Megan <mleavens19@gmail.com>
2022-03-15 20:29:42 -0400

    copying jaws_db pdo

--- config/Database.php
+++ config/Database.php
@@ -10,22 +10,22 @@ class Database {
     public function connect(){
         $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
-
-        $hostname = getenv('host');
-        $username = getenv('user');
-        $password = getenv('pass');
-        $database = getenv('path');
+        
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
 
         try {
             $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
-        }
+            }
         catch(PDOException $e)
-        {
+            {
             echo "Connection failed: " . $e->getMessage();
-        } 
+            }
 }
 
 }
