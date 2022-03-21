b9dd0a34a62522500762096c6f2480871181690b
Megan <mleavens19@gmail.com>
2022-03-21 12:15:21 -0400

    commit

--- api/authors/index.php
+++ api/authors/index.php
@@ -6,6 +6,9 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}if($method === 'GET'){
+    require_once 'read.php';
+    include_once 'read_single.php';
 }
 
 ?>
