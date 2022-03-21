539c5acbe5c8bdc5b18edeaafbdc49ba47a2c859
Megan <mleavens19@gmail.com>
2022-03-21 17:44:06 -0400

    commit

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -1,5 +1,6 @@
 <?php
 
+
 //instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
@@ -16,6 +17,16 @@ $quote->authorId = $data->authorId;
 $quote->categoryId = $data->categoryId;
 
 //create post
+$result = $quote->create();
+if($quote->authorId !== null && $quote->categoryId !== null && $quote->id !== null){
+    echo json_encode($result);
+}elseif($quote->authorId === null){
+    echo json_encode(
+        array('message' => 'authorId Not Found'));
+}else if($quote->categoryId === null){
+    echo json_encode(
+        array('message' => 'categoryId Not Found'));
+}
 $result = $quote->create();
 echo json_encode($result);
 
