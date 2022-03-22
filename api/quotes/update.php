14159a37edd1813e644e7bdd552823858566eb93
Megan <mleavens19@gmail.com>
2022-03-22 16:42:31 -0400

    commit

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -11,6 +11,12 @@ $quote = new Quote($db);
 //get raw posted data
 $data = json_decode(file_get_contents('php://input'));
 
+$data->id = htmlspecialchars(strip_tags($data->id));
+$data->quote = htmlspecialchars(strip_tags($data->quote));
+$data->category = htmlspecialchars(strip_tags($data->category));
+$data->author = htmlspecialchars(strip_tags($data->author));
+
+
 if(isset($data->id) && !empty($data->id) && isset($data->quote) && !empty($data->quote) && isset($data->category) && !empty($data->category) && isset($data->author) && !empty($data->author) ){
     $result = $quote->update();
     echo json_encode($result);
