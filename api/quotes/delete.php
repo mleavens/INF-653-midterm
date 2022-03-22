ea5b4a7e9ba01eafe2b8abee2682b76880f0fce7
Megan <mleavens19@gmail.com>
2022-03-22 16:55:49 -0400

    commit

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -15,7 +15,7 @@ $data->quote = htmlspecialchars(strip_tags($data->quote));
 $data->categoryId = htmlspecialchars(strip_tags($data->categoryId));
 $data->authorId = htmlspecialchars(strip_tags($data->authorId));
 
-if(isset($data->id) && !empty($data->id) && isset($data->quote) && !empty($data->quote)&&isset($data->authorId) && !empty($data->authorId) && isset($data->categoryId) && !empty($data->categoryId)){
+if(isset($data->quote) && !empty($data->quote)&&isset($data->authorId) && !empty($data->authorId) && isset($data->categoryId) && !empty($data->categoryId)){
     $result = $quote->delete();
     echo json_encode($result);
 }else {
