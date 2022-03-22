c2345735005c13c15c430668807da1b03a207951
Megan <mleavens19@gmail.com>
2022-03-22 09:06:11 -0400

    commit

--- api/authors/create.php
+++ api/authors/create.php
@@ -11,18 +11,27 @@ $author = new Author($db);
 //get raw posted data
 $data = json_decode(file_get_contents('php://input'));
 
-
-$author->author = $data->author;
-$author->id = $data->id;
-
-if($author->author === null){
+if(isset($data->author) && !empty($data->author)){
+    $result = $author->create();
+    echo json_encode($result);
+}else {
     echo json_encode(
         array('message' => 'Missing Required Parameters') 
         );
 }
 
-$result = $author->create();
-echo json_encode($result);
+
+// $author->author = $data->author;
+// $author->id = $data->id;
+
+// if($author->author === null){
+//     echo json_encode(
+//         array('message' => 'Missing Required Parameters') 
+//         );
+// }
+
+// $result = $author->create();
+// echo json_encode($result);
 
 
 //create post
