b09f84fbab659907f6ad1ca08e5d7c79a6458f14
Megan <mleavens19@gmail.com>
2022-03-22 09:44:22 -0400

    commit

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -10,7 +10,7 @@ $quote = new Quote($db);
 //get raw posted data
 $data = json_decode(file_get_contents('php://input'));
 
-if(isset($data->id) && !empty($data->id)){
+if(isset($data->quote) && !empty($data->quote)&&isset($data->author) && !empty($data->author) && isset($data->category) && !empty($data->category)){
     $result = $quote->delete();
     echo json_encode($result);
 }else {
