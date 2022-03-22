7fd4a4b951c2627efd5ea2abf0473f186dc7afd4
Megan <mleavens19@gmail.com>
2022-03-22 07:52:15 -0400

    commit

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../../function/isValid.php';
 
 $database = new Database();
 $db = $database->connect();
@@ -12,16 +13,24 @@ $data = json_decode(file_get_contents('php://input'));
 //Set ID to update
 $quote->id = $data->id;
 
+$quote->delete();
 
-//Delete post
-if($quote->id !== null) {
-    echo json_encode(
-        array('id' =>  $quote->id));
+$quoteExists = isValid($quote->id, $quote);
+
+if(!$quoteExists) {
+    echo json_encode(array('message' => 'No Quotes Found'));
 } else {
-    echo json_encode(
-        array('message' => 'No Quotes Found') 
-    );
+    echo json_encode(array('id' => $quote->id));
 }
+//Delete post
+// if($quote->id !== null) {
+//     echo json_encode(
+//         array('id' =>  $quote->id));
+// } else {
+//     echo json_encode(
+//         array('message' => 'No Quotes Found') 
+//     );
+// }
 
 
 exit();
