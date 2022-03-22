ff3d2c28ff33487a69fd7f0533cfa506b8118d72
Megan <mleavens19@gmail.com>
2022-03-22 09:23:06 -0400

    fixing put requirements for categories

--- api/categories/update.php
+++ api/categories/update.php
@@ -4,23 +4,32 @@
 $database = new Database();
 $db = $database->connect();
 
-//instantiate author object
+//instantiate category object
 $category = new Category($db);
 
 //get raw posted data
 $data = json_decode(file_get_contents('php://input'));
 
-//Set ID to update
-$category->id = $data->id;
+if(isset($data->id) && !empty($data->id) && isset($data->category) && !empty($data->category) ){
+    $result = $category->create();
+    echo json_encode($result);
+}else {
+    echo json_encode(
+        array('message' => 'Missing Required Parameters') 
+        );
+}
 
-$category->category = $data->category;
-$category->id = $data->id;
+// //Set ID to update
+// $category->id = $data->id;
 
-//check if quote exists
-$categoryExists = isValid($category->id, $category);
+// $category->category = $data->category;
+// $category->id = $data->id;
+
+// //check if quote exists
+// $categoryExists = isValid($category->id, $category);
 
 
-//update category
-$result = $category->update();
-echo json_encode($result);
+// //update category
+// $result = $category->update();
+// echo json_encode($result);
 ?>
