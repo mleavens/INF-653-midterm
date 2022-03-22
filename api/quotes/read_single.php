981cb318fbbe12f4da532db83b660adca06ed289
Megan <mleavens19@gmail.com>
2022-03-22 16:25:11 -0400

    commit

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -3,9 +3,37 @@
 $database = new Database();
 $db = $database->connect();
 
-$quote = new quote($db);
+$quote = new Quote($db);
 
 $quote->id = isset($_GET['id']) ? $_GET['id'] : die();
+$quote->categoryId = isset($_GET['categoryid']) ? $_GET['categoryId'] : die();
+
+$result = $quote->categoryIdQuotes();
+
+$num = $result->rowCount();
+
+if($num > 0) {
+    $quote_arr = array();
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $quote_item = array( 
+            
+            'quote' => html_entity_decode($quote),
+            'author' => $author,
+            'id' => $id,
+            'category' => $category
+        
+        );
+        array_push($quote_arr, $quote_item); 
+    }
+    print_r(json_encode($quote_arr));   
+} else {
+    echo json_encode(
+        array('message' => 'No quotes found')
+    );
+}
 
 $quote->read_single();
 
