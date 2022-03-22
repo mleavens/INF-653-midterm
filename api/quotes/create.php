1ec04847f7c3b6b06d6a432486a860a1633e0ce0
Megan <mleavens19@gmail.com>
2022-03-22 16:47:08 -0400

    message

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -11,8 +11,12 @@ $quote = new Quote($db);
 //get raw posted data
 $data = json_decode(file_get_contents('php://input'));
 
+$data->quote = htmlspecialchars(strip_tags($data->quote));
+$data->categoryId = htmlspecialchars(strip_tags($data->categoryId));
+$data->authorId = htmlspecialchars(strip_tags($data->authorId));
+
 //Check for required parameters
-if(isset($data->quote) && !empty($data->quote) && isset($data->author) && !empty($data->author) && isset($data->category) && !empty($data->category)){
+if(isset($data->id) && !empty($data->id) && isset($data->quote) && !empty($data->quote) && isset($data->categoryId) && !empty($data->categoryId) && isset($data->authorId) && !empty($data->authorId)) {
     $result = $quote->create();
     echo json_encode($result);
 }else{
